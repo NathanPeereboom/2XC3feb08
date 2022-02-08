04929a39200be8715c69a6d570b5e821f99ee778
Nathan Peereboom <npeereboom02@gmail.com>
2022-02-08 16:51:10 -0500

    Added bottom-up tests

--- code.py
+++ code.py
@@ -24,18 +24,53 @@ def randomAverageTimeTest(times,function,list_size):
     return total/int(times)
 
 
-def displayMergesort():
-    sqs = randomAverageTimeTest(500, mergesort, 100)
+def bottomUpTest():
+    # n=100
+    ts = randomAverageTimeTest(500, mergesort, 100)
     # n=400
-    sqm = randomAverageTimeTest(500, mergesort, 400)
+    tm = randomAverageTimeTest(500, mergesort, 400)
     # n=950
-    sql = randomAverageTimeTest(500, mergesort, 950)
+    tl = randomAverageTimeTest(500, mergesort, 950)
 
-    iqs = randomAverageTimeTest(500, mergesort_three, 100)
+    # n=100
+    bs = randomAverageTimeTest(500, mergesort_bottom, 100)
     # n=400
-    iqm = randomAverageTimeTest(500, mergesort_three, 400)
+    bm = randomAverageTimeTest(500, mergesort_bottom, 400)
     # n=950
-    iql = randomAverageTimeTest(500, mergesort_three, 950)
+    bl = randomAverageTimeTest(500, mergesort_bottom, 950)
+    
+    wb = Workbook()
+    sheet = wb.active
+    sheet["A1"] = "List Size (n)"
+    sheet["A2"] = "n = 100"
+    sheet["A3"] = "n = 400"
+    sheet["A4"] = "n = 950"
+
+    sheet["B1"] = "Top-down mergesort"
+    sheet["B2"] = ts
+    sheet["B3"] = tm
+    sheet["B4"] = tl
+
+    sheet["C1"] = "Bottom-up mergesort"
+    sheet["C2"] = bs
+    sheet["C3"] = bm
+    sheet["C4"] = bl
+
+    wb.save(filename="topVdown data.xlsx")
+
+
+def displayMergesort():
+    mss = randomAverageTimeTest(500, mergesort, 100)
+    # n=400
+    msm = randomAverageTimeTest(500, mergesort, 400)
+    # n=950
+    msl = randomAverageTimeTest(500, mergesort, 950)
+
+    m3s = randomAverageTimeTest(500, mergesort_three, 100)
+    # n=400
+    m3m = randomAverageTimeTest(500, mergesort_three, 400)
+    # n=950
+    m3l = randomAverageTimeTest(500, mergesort_three, 950)
 
     wb = Workbook()
     sheet = wb.active
@@ -44,15 +79,15 @@ def displayMergesort():
     sheet["A3"] = "n = 400"
     sheet["A4"] = "n = 950"
 
-    sheet["B1"] = "Standard Quicksort"
-    sheet["B2"] = sqs
-    sheet["B3"] = sqm
-    sheet["B4"] = sql
+    sheet["B1"] = "Mergesort"
+    sheet["B2"] = mss
+    sheet["B3"] = msm
+    sheet["B4"] = msl
 
-    sheet["C1"] = "In Place Sort"
-    sheet["C2"] = iqs
-    sheet["C3"] = iqm
-    sheet["C4"] = iql
+    sheet["C1"] = "Three-way mergesort"
+    sheet["C2"] = m3s
+    sheet["C3"] = m3m
+    sheet["C4"] = m3l
 
     wb.save(filename="mergesort data.xlsx")
 
@@ -97,3 +132,4 @@ def worstCase():
 # Test Test
 displayMergesort()
 worstCase()
+bottomUpTest()
